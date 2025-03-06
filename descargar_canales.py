ce03b7441e019e4db0d5ccbeb73ade2d11f6b783
ezdakit <ezdakit@gmail.com>
2025-03-06 14:16:30 +0100

    Update descargar_canales.py

--- descargar_canales.py
+++ descargar_canales.py
@@ -121,11 +121,11 @@ try:
     # Confirmar los cambios
     conn.commit()
 
-    # Generar el archivo zz_lista_ott.m3u
+    # Generar el archivo zz_lista_ott.m3u ordenado por iptv_group_new y name_new
     with open('zz_lista_ott.m3u', 'w', encoding='utf-8') as m3u_file:
         m3u_file.write('#EXTM3U url-tvg="https://raw.githubusercontent.com/davidmuma/EPG_dobleM/refs/heads/master/guiatv.xml"\n')
         m3u_file.write('#EXTVLCOPT:network-caching=2000\n\n')
-        for row in cursor.execute('SELECT iptv_epg_id_new, iptv_group_new, name_new, iptv_url FROM canales_iptv_temp WHERE activo = 1'):
+        for row in cursor.execute('SELECT iptv_epg_id_new, iptv_group_new, name_new, iptv_url FROM canales_iptv_temp WHERE activo = 1 ORDER BY iptv_group_new, name_new'):
             iptv_epg_id_new, iptv_group_new, name_new, iptv_url = row
             m3u_file.write(f'#EXTINF:-1 tvg-id="{iptv_epg_id_new}" group-title="{iptv_group_new}", {name_new}\n')
             m3u_file.write(f'{iptv_url}\n')
