98c01bb4fae240d5e5554798a2ec89b64761d6a9
ezdakit <ezdakit@gmail.com>
2025-03-10 18:05:24 +0100

    Update descargar_eventos.py
    aumenta el tiempo de espera a 30 segundos para dar más tiempo a que la tabla se cargue.

--- descargar_eventos.py
+++ descargar_eventos.py
@@ -33,8 +33,8 @@ try:
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    # Esperar a que la tabla se cargue
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'table')))
+    # Aumentar el tiempo de espera a 30 segundos
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, 'table')))
     logging.info("Tabla encontrada en la página web.")
 
     # Obtener el contenido de la página
