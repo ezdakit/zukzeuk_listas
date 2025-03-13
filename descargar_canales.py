8e56b57f88718fdc901adbeedbd5b811b7d92aad
ezdakit <ezdakit@gmail.com>
2025-03-13 10:26:16 +0100

    Update descargar_canales.py

--- descargar_canales.py
+++ descargar_canales.py
@@ -149,13 +149,22 @@ try:
 
     conn.commit()
 
-    with open('zz_lista_ott.m3u', 'w', encoding='utf-8') as m3u_file:
-        m3u_file.write('#EXTM3U url-tvg="https://raw.githubusercontent.com/davidmuma/EPG_dobleM/refs/heads/master/guiatv.xml"\n')
-        m3u_file.write('#EXTVLCOPT:network-caching=2000\n\n')
-        for row in cursor.execute('SELECT iptv_epg_id_new, iptv_group_new, name_new, iptv_url FROM canales_iptv_temp WHERE activo = 1 ORDER BY iptv_group_new, name_new'):
-            iptv_epg_id_new, iptv_group_new, name_new, iptv_url = row
-            m3u_file.write(f'#EXTINF:-1 tvg-id="{iptv_epg_id_new}" group-title="{iptv_group_new}", {name_new}\n')
-            m3u_file.write(f'{iptv_url}\n')
+    # Generar el archivo zz_lista_ott.m3u
+    try:
+        with open('zz_lista_ott.m3u', 'w', encoding='utf-8') as m3u_file:
+            m3u_file.write('#EXTM3U url-tvg="https://raw.githubusercontent.com/davidmuma/EPG_dobleM/refs/heads/master/guiatv.xml"\n')
+            m3u_file.write('#EXTVLCOPT:network-caching=2000\n\n')
+            for row in cursor.execute('SELECT iptv_epg_id_new, iptv_group_new, name_new, iptv_url FROM canales_iptv_temp WHERE activo = 1 ORDER BY iptv_group_new, name_new'):
+                iptv_epg_id_new, iptv_group_new, name_new, iptv_url = row
+                m3u_file.write(f'#EXTINF:-1 tvg-id="{iptv_epg_id_new}" group-title="{iptv_group_new}", {name_new}\n')
+                m3u_file.write(f'{iptv_url}\n')
+            # Luego escribimos los registros con activo = 0
+            for row in cursor.execute('SELECT iptv_epg_id_original, name_original, iptv_url FROM canales_iptv_temp WHERE activo = 0 ORDER BY name_original'):
+                iptv_epg_id_original, name_original, iptv_url = row
+                m3u_file.write(f'#EXTINF:-1 tvg-id="{iptv_epg_id_original}" group-title="VARIOS", {name_original}\n')
+                m3u_file.write(f'{iptv_url}\n')
+    except Exception as e:
+        logging.error(f"Error al generar zz_lista_ott.m3u: {e}")
 
     # Generar el archivo zz_lista_ace.m3u
     try:
