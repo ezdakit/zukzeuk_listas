64c8c7973f1464c3c93935558cfb0a64f49eb1c7
ezdakit <ezdakit@gmail.com>
2025-03-21 00:31:43 +0100

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+def fetch_final_content(url, timeout=5):
+    previous_content = None
+    start_time = time.time()
+
+    while True:
+        response = requests.get(url)
+        current_content = response.text
+
+        if current_content != previous_content:
+            previous_content = current_content
+            start_time = time.time()
+        elif time.time() - start_time >= timeout:
+            break
+
+        time.sleep(1)
+
+    print(current_content)
+
+if __name__ == "__main__":
+    url = "http://127.0.0.1:43110/18cZ4ehTarf34TCxntYDx9T2NHXiBvsVie/?wrapper_nonce=36c675088d663a7f4bc575928f5924ff5bdc2301b739cfc3c752b6d91dbbe011"
+    fetch_final_content(url)
