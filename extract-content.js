100003297d5f7caafba7770558b76207029b6421
ezdakit <ezdakit@gmail.com>
2025-03-22 20:38:34 +0100

    Update extract-content.js

--- extract-content.js
+++ extract-content.js
@@ -1,18 +1,21 @@
-const puppeteer = require('puppeteer');
+const { chromium } = require('playwright'); // Importar Playwright
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  // Lanzar el navegador Chromium
+  const browser = await chromium.launch();
   const page = await browser.newPage();
   const url = 'http://127.0.0.1:43110/18cZ4ehTarf34TCxntYDx9T2NHXiBvsVie/';
-  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  // Navegar a la URL
+  await page.goto(url, { waitUntil: 'networkidle' });
 
   // Esperar a que el iframe se cargue
   await page.waitForSelector('#inner-iframe');
 
   // Obtener el contenido del iframe
-  const frameHandle = await page.$('#inner-iframe');
-  const frame = await frameHandle.contentFrame();
+  const iframeHandle = await page.$('#inner-iframe');
+  const frame = await iframeHandle.contentFrame();
 
   // Esperar a que el contenido dinámico se cargue
   await frame.waitForSelector('body');
@@ -23,5 +26,6 @@ const fs = require('fs');
   // Guardar el contenido en un archivo
   fs.writeFileSync('final-content.html', finalContent);
 
+  // Cerrar el navegador
   await browser.close();
 })();
