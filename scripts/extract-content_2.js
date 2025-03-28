190ef3c4f1d22bede772f7225d5a719a30847571
ezdakit <ezdakit@gmail.com>
2025-03-28 01:49:44 +0100

    Update extract-content_2.js

--- scripts/extract-content_2.js
+++ scripts/extract-content_2.js
@@ -1,31 +1,32 @@
 const { chromium } = require('playwright');
-const fs = require('node:fs/promises');
 
-async function downloadZeroNetContent(zeronetAddress, outputFilename) {
+// Obtener los parámetros
+const zeronetAddress = process.argv[2];
+
+if (!zeronetAddress) {
+  console.error('Error: Falta el parámetro de la dirección de ZeroNet. Uso: node extract-content.js <zeronet-address>');
+  process.exit(1);
+}
+
+const zeronetUrl = `http://127.0.0.1:43110/${zeronetAddress}/`;
+
+(async () => {
+  console.log('Iniciando el script...');
+  console.log(`Navegando a ${zeronetUrl}...`);
+
+  // Lanzar el navegador Chromium
   const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  const url = `http://127.0.0.1:43110/${zeronetAddress}`;
+  const page = await browser.newPage();
 
   try {
-    console.log(`Navegando a ${url}...`);
-    await page.goto(url, { timeout: 60000 }); // Aumento del tiempo de espera por posible latencia
-
-    console.log('Extrayendo contenido HTML...');
-    const htmlContent = await page.content();
-
-    console.log(`Guardando contenido HTML en ${outputFilename}...`);
-    await fs.writeFile(outputFilename, htmlContent, 'utf8');
-    console.log(`Contenido HTML guardado en ${outputFilename}`);
-
+    // Intentar cargar la página
+    await page.goto(zeronetUrl, { waitUntil: 'load', timeout: 60000 });
+    console.log('Página cargada correctamente.');
   } catch (error) {
     console.error(`Ha ocurrido un error: ${error}`);
   } finally {
+    // Cerrar el navegador
     await browser.close();
+    console.log('Navegador cerrado.');
   }
-}
-
-const zeronetAddress = '13eNqJiWACUUuFM37xwUwmRiCuyMd6X2tS';
-const outputFilename = 'eventos_2.html';
-
-downloadZeroNetContent(zeronetAddress, outputFilename);
+})();
